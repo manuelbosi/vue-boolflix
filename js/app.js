6d14da1c0a8d1371b07536b33843616960f51252
Manuel Bosi <manuelbosi57@gmail.com>
2021-02-21 00:25:49 +0100

    creazione layout + responsive

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const apiKey = "bfbce0b99d527275ec8e7cd01aa2103d";
+
+new Vue({
+    el: "#app",
+    data : {
+
+    },
+
+    methods : {
+
+    },
+
+    mounted: function() {
+        console.log("loaded")
+    }
+})
