246ef4a850d6252d825aa1edc526daa2335b956b
Manuel Bosi <manuelbosi57@gmail.com>
2021-02-21 16:28:25 +0100

    inserimento chiamata api film popolari + info on hover

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
-const apiKey = "bfbce0b99d527275ec8e7cd01aa2103d";
+const apiKey = "api_key=bfbce0b99d527275ec8e7cd01aa2103d";
+const popular = "https://api.themoviedb.org/3/movie/popular?";
 
 new Vue({
     el: "#app",
     data : {
+        list :  [],
+        generalPosterPath : "https://image.tmdb.org/t/p/w185/",
+
 
     },
 
     methods : {
-
+        loadPopular() {
+            axios.get(popular + apiKey)
+                .then(popularMovie => {
+                this.list = popularMovie.data.results;
+                    console.log(popularMovie.data.results)
+                for (let i = 0; i < popularMovie.data.results.length; i++) {
+                    const el = popularMovie.data.results[i];
+                    el.vote_average = Math.ceil(el.vote_average / 2)
+                }
+            })
+        }
     },
 
     mounted: function() {
-        console.log("loaded")
+        this.loadPopular();
+
     }
 })
